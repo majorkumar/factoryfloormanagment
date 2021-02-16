1e41c553ef8539335d4e61b9afb8b39c3bdcef8e
Amit Singh <58631973+majorkumar@users.noreply.github.com>
2021-02-16 21:01:33 +0530

    Update client.py

--- broker_mqtt/client.py
+++ broker_mqtt/client.py
@@ -7,13 +7,13 @@ def on_message(client, userdata, message):
 	print("msg qos=", message.qos)
 	print("msg retain flag= ", message.retain)
 
-borker_address = "192.168.75.170"
+borker_address = "192.168.11.111"
 
 print("creating new instance ")
 client = mqtt.Client("P1")
 while(1):
 	client.on_message=on_message
-	client.connect("192.168.75.170")
+	client.connect("192.168.11.111")
 	client.loop_start()
 	print("subscribing to topic", "esp32")
 	client.subscribe("esp32")
